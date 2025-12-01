20e16dcb4633c2c49ae4e3795547f1a38f10245b
Srishti Singh <srish17816@gmail.com>
2025-12-01 09:28:17 +0530

    userSchema done

--- src/model/User.model.ts
+++ src/model/User.model.ts
@@ -18,3 +18,53 @@ const MessageSchema : Schema<Message> = new Schema({
     }
 
 })
+
+export interface User extends Document{
+    username : string
+    email : string
+    password : string
+    verifyCode : string
+    verifyCodeExpiry : Date
+    isVerified : boolean
+    isAcceptingMessage : boolean
+    messages : Message[]
+}
+
+const UserSchema : Schema<User> = new Schema({
+    username : {
+        type : String,
+        required : [true, "Username is required"],
+        trim : true,
+        unique : true
+    },
+    email : {
+        type : String,
+        required : [true, "Username is required"],
+        unique : true,
+        match : [/^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/, 'please use valid email']
+    },
+    password : {
+        type : String,
+        required : [true, "Password is required"],
+    },
+    verifyCode : {
+        type : String,
+        required : [true, "VerifyCoode is required"],
+    },
+    verifyCodeExpiry : {
+        type : Date,
+        required : [true, "verifyCodeExpiry is required"],
+        
+    },
+    isVerified : {
+        type : Boolean,
+        default : false
+    },
+    isAcceptingMessage : {
+        type : Boolean,
+        default : true
+    },
+    messages : {
+        type : [MessageSchema] // array
+    }
+})
